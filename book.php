566ca245ce24d685b39bce5623fef12b4329b3f7
Giovanni Canzio <gc@grin.com>
2014-02-28 11:14:10 +0100

    base skeleton structure

---
+++ book.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
+<head>
+
+	<!-- Basic Page Needs
+  ================================================== -->
+	<meta charset="utf-8">
+	<title>Your Page Title Here :)</title>
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<!-- Mobile Specific Metas
+  ================================================== -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<!-- CSS
+  ================================================== -->
+	<link rel="stylesheet" href="stylesheets/base.css">
+	<link rel="stylesheet" href="stylesheets/skeleton.css">
+	<link rel="stylesheet" href="stylesheets/layout.css">
+
+	<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
+	<!-- Favicons
+	================================================== -->
+	<link rel="shortcut icon" href="images/favicon.ico">
+	<link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
+
+</head>
+<body class="book-page">
+
+
+
+	<!-- Primary Page Layout
+	================================================== -->
+
+	<!-- Delete everything in this .container and get started on your own site! -->
+
+	<div class="container">
+		<nav class="mainNav">
+			<ul>
+				<li><a href="index.php">Gewinnspiel</a></li>
+				<li><a href="book.php">Buch</a></li>
+			</ul>
+		</nav>
+
+		<h1 class="heading heading1">Die Frauen von Tyringham Park</h1>
+
+		<div class="content">
+			<img src=""
+			<ul class="secNav">
+				<li><a href="#" class="left">H&ouml;rprobe</a></li>		
+				<li><a href="#" class="left">Zum H&ouml;rbuch</a></li>		
+				<li><a href="#" class="left">Auch als H&ouml;rbuch-download</a></li>		
+				<li><a href="#" class="right">Leseprobe</a></li>		
+				<li><a href="#" class="right">Zum Buch</a></li>		
+				<li><a href="#" class="right">Auch als E-Book</a></li>		
+			</ul>
+			<p>Aenean rhoncus, elit sed ultrices tempor, nisl nisl rhoncus sapien, a luctus turpis ante sed urna.<br />
+			Sed venenatis laoreet arcu, porta posuere ipsum fringilla a. Duis vestibulum non libero eu pharetra. Pellentesque adipiscing, ipsum at accumsan aliquam.</p>
+			<ul class="bottomNav">
+				<li><a href="#">Teilen</a></li>
+			</ul>
+		</div>
+	</div><!-- container -->
+
+
+<!-- End Document
+================================================== -->
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
+<head>
+
+	<!-- Basic Page Needs
+  ================================================== -->
+	<meta charset="utf-8">
+	<title>Your Page Title Here :)</title>
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<!-- Mobile Specific Metas
+  ================================================== -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<!-- CSS
+  ================================================== -->
+	<link rel="stylesheet" href="stylesheets/base.css">
+	<link rel="stylesheet" href="stylesheets/skeleton.css">
+	<link rel="stylesheet" href="stylesheets/layout.css">
+
+	<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
+	<!-- Favicons
+	================================================== -->
+	<link rel="shortcut icon" href="images/favicon.ico">
+	<link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
+
+</head>
+<body class="home-page">
+
+
+
+	<!-- Primary Page Layout
+	================================================== -->
+
+	<!-- Delete everything in this .container and get started on your own site! -->
+
+	<div class="container">
+		<nav class="mainNav">
+			<ul>
+				<li><a href="index.php">Gewinnspiel</a></li>
+				<li><a href="book.php">Buch</a></li>
+			</ul>
+		</nav>
+		<h1 class="heading heading1">Leidenschaft und Intrigen auf Tyringham Park</h1>
+		<div class="introText">
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+			<p>Aliquam aliquet nec ligula et volutpat. Sed ornare lorem et nisl gravida, eu aliquet metus bibendum. Mauris mollis nibh a quam bibendum tincidunt.</p>
+		</div>
+		<div class="content">
+			<h2 class="heading heading2">Gewinnspiel</h2>
+			<p>Aenean rhoncus, elit sed ultrices tempor, nisl nisl rhoncus sapien, a luctus turpis ante sed urna.</p>
+			<p class="highlight">Nullam sit amet sapien est. Donec tincidunt ornare velit?</p>
+			<p>Sed venenatis laoreet arcu, porta posuere ipsum fringilla a. Duis vestibulum non libero eu pharetra. Pellentesque adipiscing, ipsum at accumsan aliquam.</p>
+			<ul class="bottomNav">
+				<li><a href="#">Mitmachen</a></li>
+				<li><a href="#">Teilen</a></li>
+			</ul>
+		</div>
+	</div><!-- container -->
+
+
+<!-- End Document
+================================================== -->
+</body>
+</html>
