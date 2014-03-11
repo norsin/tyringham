ee083b68e023ae2e40c1a44066e2b78c1ececbfd
norsin <nt@bilandia.de>
2014-03-11 10:56:14 +0100

    Customer-Changes Teilnahmebox

--- teilnahmebox.php
+++ teilnahmebox.php
@@ -8,6 +8,7 @@
 	Voraussetzung für die Teilnahme am Gewinnspiel ist das Klicken auf den „Gefällt mir“-Button der Bastei Lübbe- oder Lübbe Audio-Facebook-Seite sowie eine E-Mail mit der richtigen Lösung an <span class="desktop-content">facebook@luebbe.de.</span><span class="tablet-content phone-content">gewinnspiel@luebbe.de.</span> Ausgeschlossen von der Teilnahme sind Mitarbeiter und Agenturen der Bastei Lübbe AG, sowie angeschlossener Unternehmen und deren Angehörige.
 	<br />
 	Die Teilnahme ist nur natürlichen Personen gestattet. Eine Sammelteilnahme (insbesondere unter Einbeziehung hierauf ausgerichteter Unternehmen) ist ausdrücklich ausgeschlossen. Teilnehmer müssen persönlich mitspielen und dürfen sich keinerlei technischer Hilfsmittel bedienen. Besteht der Verdacht, dass doch Hilfsprogramme o. ä. benutzt worden sind, behält sich der Verlag vor, die Mitspieler von der Teilnahme auszuschließen.</p>
+	<br /><br />
 	<p>
 	<u>
 	Gewinne
@@ -23,6 +24,7 @@
 	<br /><br />
 	Die Preise werden unter allen Einsendungen verlost. Das Gewinnspiel beginnt am 12.03.2014 und endet am 13.04.2014. Die Gewinner werden nach Ablauf des Gewinnspiels per E-Mail benachrichtigt. Zum Abruf des Gewinns geben die Gewinner per E-Mail an <span class="desktop-content">facebook@luebbe.de</span><span class="tablet-content phone-content">gewinnspiel@luebbe.de</span> ihren vollständigen Namen sowie ihre Adresse bekannt.
 	</p>
+	<br /><br />
 	<p>
 	<u>
 	Datenschutz
@@ -34,6 +36,7 @@
 	<br /><br />
 	Die uns übermittelten Daten werden nur für das Gewinnspiel und zur Benachrichtigung der Gewinner verwendet. Die namentliche Veröffentlichung endet nach vollständiger Beendigung und Abwicklung des Gewinnspiels. Die gespeicherten Daten werden anschließend gelöscht, soweit ein Teilnehmer nicht seine Einwilligung zur weiteren Nutzung der Daten erteilt hat.
 	</p>
+	<br /><br />
 	<p>
 	<u>
 	Nutzung von Daten durch Facebook
@@ -41,6 +44,7 @@
 	<br /><br />
 	Für unser Gewinnspiel nutzen wir die Plattform des sozialen Netzwerks facebook.com, das von der Facebook Ireland Ltd., Hanover Reach 5-7, Hanover Quay, Dublin 2 Irland, betrieben wird („Facebook“). Durch Ihre Teilnahme an diesem Gewinnspiel ist es nicht auszuschließen, dass Facebook Kenntnis über Ihre IP-Adresse erlangt. Alle weiteren Daten, die im Rahmen dieses Gewinnspiels erfasst werden, werden nicht an Facebook oder andere Dritte weitergegeben. Zweck und Umfang der Datenerhebung und die weitere Verarbeitung und Nutzung der Daten durch Facebook sowie ihre diesbezüglichen Rechte und Einstellungsmöglichkeiten zum Schutz Ihrer Privatsphäre entnehmen Sie bitte den Datenschutzhinweisen von Facebook: http://www.facebook.com/policy.php
 	</p>
+	<br /><br />
 	<p>
 	<u>
 	Freistellung von Facebook
@@ -49,9 +53,4 @@
 	Der Rechtsweg ist ausgeschlossen. Veranstalter des Gewinnspiels ist allein die Bastei Lübbe AG. Das Gewinnspiel steht in keiner Verbindung zu Facebook und wird in keiner Weise von Facebook gesponsert, unterstützt oder organisiert. Mit der Teilnahme am Gewinnspiel akzeptieren die Teilnehmer die vorstehenden Bedingungen.
 	</p>
 	<p><div class="close" id="close2">Fenster schließen</div></p>
-	<div class="box ausschluss">
-		<span style="margin-top: 10px;"> 
-		Veranstalter des Gewinnspiels ist allein die Bastei Lübbe AG. Das Gewinnspiel steht in keiner Verbindung zu Facebook und wird in keiner Weise von Facebook gesponsert, unterstützt oder organisiert. Das Gewinnspiel wird durch die bilandia GmbH ausgeführt.
-		</span>
-	</div>
 </div>
